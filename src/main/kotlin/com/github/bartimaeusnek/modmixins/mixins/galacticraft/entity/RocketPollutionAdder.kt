e2b8c9a2062da0798d22c5402a87caeee817fefc
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2020-06-16 21:47:07 +0200

    added additional checks to improve performance

--- src/main/kotlin/com/github/bartimaeusnek/modmixins/mixins/galacticraft/entity/RocketPollutionAdder.kt
+++ src/main/kotlin/com/github/bartimaeusnek/modmixins/mixins/galacticraft/entity/RocketPollutionAdder.kt
@@ -16,14 +16,13 @@ abstract class RocketPollutionAdder(world: World) : EntityAutoRocket(world), IRo
 
     @Inject(method = ["onUpdate"],at =  [At("HEAD")])
     fun addRocketStartPollution(x: CallbackInfo) {
-        if (!this.worldObj.isRemote){
+        if (!this.worldObj.isRemote && (this.launchPhase == EnumLaunchPhase.LAUNCHED.ordinal || this.launchPhase == EnumLaunchPhase.IGNITED.ordinal)){
              GT_Pollution.addPollution(this.worldObj.getChunkFromBlockCoords(this.posX.toInt(),this.posZ.toInt()),
                      when (this.launchPhase){
                          EnumLaunchPhase.LAUNCHED.ordinal -> (LoadingConfig.rocketPollution shl this.rocketTier) / 20
                          EnumLaunchPhase.IGNITED.ordinal -> (LoadingConfig.rocketPollution shl this.rocketTier) / 20 / 100
                          else -> 0
                      }
-
              )
         }
     }
